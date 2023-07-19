19635cecec46de002434c17d6e9620e823780987
perymerdeka <tkjasikk@gmail.com>
2023-07-19 21:16:18 +0700

    Creating Templated

--- src/main.py
+++
@@ -1,3 +0,0 @@
-from spider.config import Config as cfg
-
-print(cfg.BASE_DIR)
--- src/spider/config.py
+++
@@ -1,7 +0,0 @@
-from os.path import join
-from pathlib import Path
-
-class Config(object):
-    BASE_DIR = Path(__file__).resolve().parent.parent.parent
-    LOGDIR = join(BASE_DIR, "log")
-    TEMP_DIR = join(BASE_DIR, "temp")
